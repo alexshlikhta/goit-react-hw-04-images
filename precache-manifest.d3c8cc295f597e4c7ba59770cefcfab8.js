45bca8b5c17d7f63c67dcf868615bba823917a93
alexshlikhta <alexshlihta@gmail.com>
2022-04-26 21:52:31 +0300

    Updates

---
+++ precache-manifest.d3c8cc295f597e4c7ba59770cefcfab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0862583cced9b5fe01cb",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.dd49ea69.chunk.css"
+  },
+  {
+    "revision": "0862583cced9b5fe01cb",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.6199200f.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  },
+  {
+    "revision": "eda5d0638ad4d9e41bd8",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b5a62cbe.chunk.js"
+  },
+  {
+    "revision": "640df5eaaa0009b98d6918040b749e82",
+    "url": "/goit-react-hw-03-image-finder/static/media/searching.640df5ea.jpeg"
+  },
+  {
+    "revision": "6ab415fd9a3a90daf428f44b686532f6",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  }
+];
